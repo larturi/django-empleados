ebd071714a23cd6dc004cd0c2acf6a2f4c181dfa
lea.arturi <lea.arturi@gmail.com>
2021-10-02 12:54:04 -0300

    Modelo Departamento

--- empleados/applications/departamento/models.py
+++ empleados/applications/departamento/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.db.models.fields import CharField
 
-# Create your models here.
+class Departamento(models.Model):
+    name = models.CharField('Nombre', max_length=50)
+    short_name = models.CharField('Nombre Corto', max_length=20, unique=True)
+    habilitado = models.BooleanField('Habilitado', default=True)
+
+    def __str__(self):
+        return str(self.id) + ' - ' + self.name
