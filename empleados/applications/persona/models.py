77de935906ec26578116511e945748538a30281c
lea.arturi <lea.arturi@gmail.com>
2021-10-02 14:56:51 -0300

    Conexion a Postgres

--- empleados/applications/persona/models.py
+++ empleados/applications/persona/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+from applications.departamento.models import Departamento
+
+class Empleado(models.Model):
+
+    JOB_CHOICES = (
+        ('0', 'Contador'),
+        ('1', 'Administrador'),
+        ('2', 'Informatico'),
+        ('3', 'Economista'),
+        ('4', 'Otro'),
+    )
+    first_name = models.CharField('Nombre', max_length=100)
+    last_name = models.CharField('Apellido', max_length=100)
+    job = models.CharField('Puesto', max_length=50, choices=JOB_CHOICES)
+    departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.id) + ' - ' + self.first_name + ' - ' + self.last_name
--- empleados/empleados/settings/local.py
+++ empleados/empleados/settings/local.py
@@ -15,8 +15,12 @@ ALLOWED_HOSTS = []
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "empleados",
+        'USER': "empleados",
+        'PASSWORD': "empleados",
+        'HOST': "localhost",
+        'PORT': "5432",
     }
 }
 
