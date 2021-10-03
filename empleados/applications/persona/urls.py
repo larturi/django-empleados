81daea9bd608e935a6107301f24cbfbd11db5450
lea.arturi <lea.arturi@gmail.com>
2021-10-03 12:54:48 -0300

    ListView Empleados

--- empleados/applications/persona/urls.py
+++ empleados/applications/persona/urls.py
@@ -5,5 +5,8 @@ from . import views
 
 urlpatterns = [
     path('listar-empleados/', views.ListEmpleados.as_view()),
-    path('listar-empleados-departamento/', views.ListEmpleadosByDepartamento.as_view()),
+    path('listar-empleados-departamento/<short_name>', views.ListEmpleadosByDepartamento.as_view()),
+    path('listar-empleados-job/<job>', views.ListEmpleadosByPuesto.as_view()),
+    path('buscar-empleado/', views.ListEmpleadosByKword.as_view()),
+    path('habilidades-empleado/<id>', views.ListHabilidadesEmpleado.as_view()),
 ]
--- empleados/applications/persona/views.py
+++ empleados/applications/persona/views.py
@@ -4,9 +4,40 @@ from .models import Empleado
 
 class ListEmpleados(ListView):
     template_name = 'persona/list_all.html'
+    paginate_by = 3
     model = Empleado
 
 
 class ListEmpleadosByDepartamento(ListView):
     template_name = 'persona/list-by-departamento.html'
-    queryset = Empleado.objects.filter(departamento__name="Marketing")
+
+    def get_queryset(self):
+        departamento = self.kwargs['short_name']
+        lista = Empleado.objects.filter(departamento__short_name=departamento.upper())
+        return lista
+
+class ListEmpleadosByPuesto(ListView):
+    template_name = 'persona/list-by-puesto.html'
+
+    def get_queryset(self):
+        job = self.kwargs['job']
+        lista = Empleado.objects.filter(job=job)
+        return lista
+
+class ListEmpleadosByKword(ListView):
+    template_name = 'persona/by_keyword.html'
+    context_object_name = 'empleados'
+
+    def get_queryset(self):
+        palabra_clave = self.request.GET.get('kword', '')
+        lista = Empleado.objects.filter(first_name=palabra_clave)
+        return lista
+
+class ListHabilidadesEmpleado(ListView):
+    template_name = 'persona/habilidades.html'
+    context_object_name = 'habilidades'
+
+    def get_queryset(self):
+        id = self.kwargs['id']
+        empleado = Empleado.objects.get(id=id)
+        return empleado.habilidades.all()
