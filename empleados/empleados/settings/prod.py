0887c506aadf30a2b67ca8c8899cb19f71d4163e
lea.arturi <lea.arturi@gmail.com>
2021-10-11 14:03:54 -0300

    Statics Prod

--- empleados/empleados/settings/prod.py
+++ empleados/empleados/settings/prod.py
@@ -28,7 +28,7 @@ DATABASES = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # STATICFILES_DIRS = [BASE_DIR / "../static",]
