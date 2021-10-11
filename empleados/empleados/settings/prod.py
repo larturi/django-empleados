b49e33c47f7159c62a43b16de958df218d075ef7
lea.arturi <lea.arturi@gmail.com>
2021-10-11 15:44:34 -0300

    Prod

--- empleados/empleados/settings/prod.py
+++ empleados/empleados/settings/prod.py
@@ -26,9 +26,12 @@ DATABASES = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "../static",]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR.parent/'media/'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
