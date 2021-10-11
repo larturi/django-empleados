0148a7499d030944b44606d082cf6566e8b39d7a
lea.arturi <lea.arturi@gmail.com>
2021-10-11 15:10:31 -0300

    Prod

--- empleados/empleados/settings/prod.py
+++ empleados/empleados/settings/prod.py
@@ -31,16 +31,10 @@ DATABASES = {
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = [BASE_DIR / "../static",]
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
 MEDIA_URL = '/media/'
-# MEDIA_ROOT = BASE_DIR.parent/'media/'
 
-MIDDLEWARE_CLASSES = (
-    # Simplified static file serving.
-    # https://warehouse.python.org/project/whitenoise/
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
